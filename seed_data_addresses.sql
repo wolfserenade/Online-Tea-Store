51508de5d31c5e0b5b1a1d3f27904665788cc687
wolfserenade <lindahuang1119@gmail.com>
2023-06-13 23:24:21 -0400

    seed data addresses

---
+++ seed_data_addresses.sql
@@ -0,0 +1,13 @@
+INSERT INTO addresses (address_id, street, city, state, postcode, country) VALUES
+	('123', 'Charmander Street', 'Jersey City', 'NJ', 12345, 'USA'),
+	('234', 'Pikachu Street', 'New York', 'NY', 10022, 'USA'),
+	('987', 'Squirtle Ave', 'Brooklyn', 'NY', 11243, 'USA'),
+	('545', 'Eeve Rd', 'Queens', 'NY', 11436, 'USA'),
+	('4657', 'Bulbasaur Ave', 'New Haven','CT', 23663, 'USA'),
+	('854', 'Snorlax Blvd', 'Hartford', 'CT', 23425, 'USA'),
+	('154', 'Onix Rd', 'Weehawken', 'NJ', 09304, 'USA'),
+	('644', 'Mew Street', 'Bronx', 'NY', 34221, 'USA'),
+	('5745' 'Lapras Ave', 'Vernon', 'NJ', 34566, 'USA'),
+	('554', 'Machop Rd', 'Staten Island', 'NY' 34443, 'USA'),
+	('643', 'Growlith Blvd', 'East Hamptons', 'NY', 37557, 'USA'),
+	('777', 'Articuno Ave', 'Union City', 'NJ' 03562, 'USA');
