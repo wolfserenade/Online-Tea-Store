5c981e5cdf185bd9372fac8e92252c92ec233a61
wolfserenade <lindahuang1119@gmail.com>
2023-07-06 22:18:05 -0400

    seed data for customers

---
+++ seed_data_customers.sql
@@ -0,0 +1,12 @@
+INSERT INTO customers (customer_id, first_name, last_name, phone_number, email) VALUES
+	('111', 'Richard', 'Kramer', '394-485-2858', 'krichard@gmail.com'),
+	('222', 'George', 'Costanza', '353-546-5343', 'gcostanza@yahoo.com'),
+	('333', 'Elaine', 'Benes', '234-553-5553', 'b_elaine@aol.com'),
+	('233', 'Wayne', 'Newman', '355,679-8667', 'newman345@gmail.com'),
+	('358', 'Jerry', 'Seinfeld', '777-899-4235', 'jerryseinfeld@yahoo.com'),
+	('788', 'Susan', 'Ross', '332-784-2246', 'ross@gmail.com'),
+	('872', 'John', 'Peterman', '869-331-5511', 'jpeterman123@gmail.com'),
+	('535', 'Jackie', 'Chiles', '113-456-6866', 'jchiles766@gmail.com'),
+	('553', 'Tim', 'Whatley', '244-663-7468', 'whatley_t25@yahoo.com'),
+	('669', 'George', 'Steinbrenner', '247-646-2570', 'gsteinbrenner550@gmail.com')
+;
